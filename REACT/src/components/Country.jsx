c5e4f29fba45788ae461edc03944eb6e6235d61e
Marco <marko.dzm@gmail.com>
2023-05-12 13:04:45 -0600

    Mayo-12 A

--- REACT/src/components/Country.jsx
+++ REACT/src/components/Country.jsx
@@ -1,9 +1,25 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Country = () => {
+
+    // El estado lo nombramos de acuerdo a lo que recibamos en la API
+    const [ counrty, setCountry ] = useState({});
+    
+    useEffect(() => {
+        axios.get('https://restcountries.com/v3.1/name/deutschland')
+             .then(res => setCountry(res.data));
+    }, [])
+
+    console.log(counrty);
+
     return (
         <div>
             <h1>HOLA MUNDO</h1>
+            <section>
+                <h2>{counrty[0]?.name.common}</h2>
+                
+            </section>
         </div>
     );
 };
